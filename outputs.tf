56421f1b0154d4e27cd3de28dc7a3f5b5cc128db
Daniel Allen <dan@aeternagroup.net>
2026-08-15 01:43:42 +0100

    Update to schema version v6.58.0

--- outputs.tf
+++ outputs.tf
@@ -4,7 +4,7 @@ output "connect_user_hierarchy_structures_id" {
 }
 output "connect_user_hierarchy_structures_hierarchy_structure" {
   description = "Map of hierarchy_structure values across all connect_user_hierarchy_structures, keyed the same as var.connect_user_hierarchy_structures"
-  value       = { for k, v in aws_connect_user_hierarchy_structure.connect_user_hierarchy_structures : k => v.hierarchy_structure if v.hierarchy_structure != null && length(v.hierarchy_structure) > 0 }
+  value       = { for k, v in aws_connect_user_hierarchy_structure.connect_user_hierarchy_structures : k => one(v.hierarchy_structure) if v.hierarchy_structure != null && length(v.hierarchy_structure) > 0 }
 }
 output "connect_user_hierarchy_structures_instance_id" {
   description = "Map of instance_id values across all connect_user_hierarchy_structures, keyed the same as var.connect_user_hierarchy_structures"
